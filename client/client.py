45d7c4cbe25d9a629fb5c6519469eaee5108f7be
lukaisailovic <lukaisailovic@gmail.com>
2022-06-05 23:19:02 +0200

    Change client script and update dockerfile

--- client/client.py
+++ client/client.py
@@ -1,9 +1,26 @@
-import http.client
+import requests
+from requests.structures import CaseInsensitiveDict
 
-conn = http.client.HTTPSConnection("localhost", 8081)
-payload = "{\r\n\t\"ime\": \"Luka\",\r\n\t\"prezime\": \"Isailovic\",\r\n\t\"smer\": \"rm\",\r\n\t\"predmeti\": [\r\n\t\t{\r\n\t\t\t\"ime\": \"pds\",\r\n\t\t\t\"espb\": 5\r\n\t\t}\r\n\t]\r\n}"
-headers = {}
-conn.request("POST", "/users", payload, headers)
-res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+url = "http://localhost:8081/users"
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+headers["Content-Type"] = "application/json"
+
+data = """
+{
+	"ime": "Luka",
+	"prezime": "Isailovic",
+	"smer": "rm",
+	"predmeti": [
+		{
+			"ime": "pds",
+			"espb": 5
+		}
+	]
+}
+"""
+
+resp = requests.post(url, headers=headers, data=data)
+
+print(resp.status_code)
